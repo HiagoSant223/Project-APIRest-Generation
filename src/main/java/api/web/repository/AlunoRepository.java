a39c3b76fef753cbf1e03964652e467d60d2984e
Hiago Oliveira <hiagosantos223@gmail.com>
2023-10-21 15:49:41 -0300

    Project created

---
+++ src/main/java/api/web/repository/AlunoRepository.java
@@ -0,0 +1,25 @@
+package api.web.repository;
+
+import api.web.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+    //Query Method
+    List<Aluno> findByNomeContaining(String nome);
+
+    //Query Method
+
+    List<Aluno> findByIdadeGreaterThan(Integer idade);
+
+    List<Aluno> findByNotaPrimeiroSemestreGreaterThan(Integer nota);
+
+    List<Aluno> findByNomeProfessor(String nomeProfessor);
+
+    @Query("SELECT a FROM Aluno a WHERE a.nome LIKE %:nome%")
+    List<Aluno> filtrarPorNome(@Param("nome") String nome);
+
+}
